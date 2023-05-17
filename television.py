b5f6bf821a2cf27cf307ff347e5cbc1e184c9527
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-17 11:59:58 +0800

    Special init method and instance variables added

--- television.py
+++ television.py
@@ -1,6 +1,11 @@
 # Create a class called TV
+class TV_Controller():
     # Create a special init method
+    def __init__(self, screen = "OFF", volume = 7 , channel = "RBS"):
         # Set all the instance variables
+        self.screen = screen
+        self.volume = volume
+        self.channel = channel
     
     # Create a method for when the television is turned on
     # Create a method for when the television is turned off
