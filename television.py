4842291e92569076ff7f6d32f9a533ce2b75634e
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-17 11:50:16 +0800

    added python file television (with pseudocode)

---
+++ television.py
@@ -0,0 +1,14 @@
+# Create a class called TV
+    # Create a special init method
+        # Set all the instance variables
+    
+    # Create a method for when the television is turned on
+    # Create a method for when the television is turned off
+    # Create a method to change the volume of the television
+    # Create a method to change the channel the television is on
+    
+    
+    
+        
+    
+    
