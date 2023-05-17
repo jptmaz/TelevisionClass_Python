b7619d34172ff495c4bb1bfc0df34eb0938eab03
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-17 23:07:03 +0800

    Instance variables added

--- television.py
+++ television.py
@@ -6,5 +6,8 @@ class tv_controller():
     min_chan = 1
     # Create a special init method
     def __init__(self, screen = "OFF", volume = 0, channel = 1):
-
+        # Select all instance variables
+        self.screen = screen
+        self.volume = volume
+        self.channel = channel
     
