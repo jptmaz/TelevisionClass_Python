e089d3e105950dabeb1a1c54405b2fabb5e83dd8
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-17 12:06:14 +0800

    ON&OFF methods have been added

--- television.py
+++ television.py
@@ -6,9 +6,20 @@ class TV_Controller():
         self.screen = screen
         self.volume = volume
         self.channel = channel
-    
     # Create a method for when the television is turned on
+    def TV_On(self):
+        if self.screen == "ON":
+            print("Your tellie is ON.")
+        else:
+            self.screen = "ON"
+            print("Your tellie is being opened.")
     # Create a method for when the television is turned off
+    def TV_close(self):
+        if self.screen == "OFF":
+            print("Your tellie os OFF.")
+        else:
+            self.screen = "OFF"
+            print("Your tellie is closing now.")
     # Create a method to change the volume of the television
     # Create a method to change the channel the television is on
     
