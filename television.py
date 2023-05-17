10f84e616a9f7c01ebfa750987914a05dfe048b0
Jasmin Paige <jaspagmaz98@gmail.com>
2023-05-18 07:16:07 +0800

    Volume of TV code completed

--- television.py
+++ television.py
@@ -16,14 +16,14 @@ class tv_controller():
         if self.screen == "ON":
             print("Tellie is on already, silly!")
         else:
-            self.screen = "ON":
-                print("The Tellie is now on! What shall we do next?")
+            self.screen = "ON"
+            print("The Tellie is now on! What shall we do next?")
     def tv_close(self):
-         if self.screen == "OFF":
+        if self.screen == "OFF":
             print("Tellie is off already, silly!")
         else:
-            self.screen = "OFF":
-                print("The Tellie is now off! It's not a good idea to spend so much time watching the Tellie, good job!")
+            self.screen = "OFF"
+            print("The Tellie is now off! It's not a good idea to spend so much time watching the Tellie, good job!")
     
     # Create method to change the volume of the TV
     def tv_volume(self):
@@ -31,11 +31,25 @@ class tv_controller():
             volume_command = input("""\\\\ CHANGE THE VOLUME LEVEL ////
                                Volume + 'UP'
                                Volume - 'DOWN'
-                               RETURN 'R'"""
-        if volume_command >= max_vol:
-            print("Wah! The Tellie is already at max volume! My ears!")
-        else:
-            self.volume 
+                               RETURN 'R'""")
+            if volume_command == ">":
+                if self.volume >= max_vol:
+                    print("Wah! The Tellie is already at max volume! My ears!")
+                else:
+                    self.volume += 1
+                    print("Volume is now " + self.volume)
+            elif volume_command == "<":
+                if self.volume <= min_vol:
+                    print("The Tellie is already at min volume! I can't hear a thing.")
+                else:
+                    self.volume -= 1
+                    print("Volume is now " + self.volume)
+            else:
+                break
+            
+                
+            
+            
         
             
         
